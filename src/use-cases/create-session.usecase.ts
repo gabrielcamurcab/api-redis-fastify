cd72543759e3cb5b6c826d8f4cb090373e4de305
Gabriel Camurça <projetosdocamurca@gmail.com>
2025-11-20 11:39:46 -0300

    feat: implements create session use case

---
+++ src/use-cases/create-session.usecase.ts
@@ -0,0 +1,10 @@
+import { SessionPort } from "../application/ports/session";
+
+export class CreateSessionUseCase {
+    constructor (private readonly sessions: SessionPort) {}
+
+    async execute(): Promise<string> {
+        const id = await this.sessions.createSession();
+        return id;
+    }
+}
