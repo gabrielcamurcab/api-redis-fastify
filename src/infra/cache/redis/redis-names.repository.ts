a463b17b08255f4951e14c9ab88ad60444d8a325
Gabriel Camurça <projetosdocamurca@gmail.com>
2025-11-20 10:57:42 -0300

    feat: implements redis names repository - adapter

---
+++ src/infra/cache/redis/redis-names.repository.ts
@@ -0,0 +1,20 @@
+import { NamesPort } from "../../../application/ports/names";
+import { RedisClient } from "./redis-client";
+
+export class RedisNamesRepository implements NamesPort {
+    private readonly NAMES_TTL = 3600; // 1H
+
+    constructor (private readonly redis: RedisClient) {}
+
+    async addName(sessionId: string, name: string): Promise<void> {
+        const key = `session:${sessionId}:names`;
+
+        await this.redis.raw.sadd(key, name);
+        await this.redis.raw.expire(key, this.NAMES_TTL);
+    }
+
+    async getNames(sessionId: string): Promise<string[]> {
+        const key = `session:${sessionId}:names`;
+        return await this.redis.raw.smembers(key);
+    }
+}
